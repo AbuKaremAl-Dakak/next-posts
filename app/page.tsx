031a005d4b462e2d800c8e221f6c80710db08544
abukarem <abukarem11311@gmail.com>
2024-08-01 23:18:26 +0300

    add dark and light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import DashboardCard from "@/components/dashboard/DashboardIcon";
 import PostsTable from "@/components/posts/PostsTable";
 import { Folders, MessageCircle, Newspaper, User } from "lucide-react";
@@ -27,7 +28,8 @@ export default function Home() {
           icon={<MessageCircle className="text-slate-500" size={72} />}
         />
       </div>
-      <PostsTable />
+      <AnalyticsChart />
+      <PostsTable title="Latest Posts" limit={5} />
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import ThemeToggler from "./ThemeToggler";
 
 const Navbar = () => {
   return (
@@ -18,25 +19,29 @@ const Navbar = () => {
         <Image src={logo} alt="web site logo" width={40}></Image>
       </Link>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <div className="flex items-center">
+        <ThemeToggler />
 
-            {/* <AvatarFallback className="text-black">BT</AvatarFallback> */}
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/auth">Logout</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+
+              {/* <AvatarFallback className="text-black">BT</AvatarFallback> */}
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
